628a67c016ed370d06a2a34959cd0a9189e63fe0
OpsecFreak <mobiletechspecialists@gmail.com>
2025-10-07 19:50:24 -0400

    Create dashboard layout with sidebar

---
+++ flightlogger-app/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardHome() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Welcome to your Dashboard</h1>
+      <p className="mb-8">Select an option from the sidebar to get started.</p>
+    </div>
+  );
+}
